71e5e71ad709b4f1ecd5f400cf2d0620d88375eb
0xakash9 <devakash256@gmail.com>
2024-10-08 18:53:18 +0530

    init

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
+import Constants from "expo-constants";
+
+export default function HomeScreen() {
+  return (
+    <WebView
+      style={styles.view}
+      source={{
+        uri: "https://www.google.com",
+      }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+    height: 800,
+    width: "100%",
+    borderRadius: 8,
+  },
+});
